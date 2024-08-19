8f7fbc5fc7706636c22f76a1491b6f7e3460ae96
Stephen Adams <stephen@cgcsoftware.co.uk>
2024-08-19 16:04:26 +0100

    feat: Add search box and playlist display components to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { SearchBoxComponent } from "../../ui/search-box/search-box.component";
+import { PlaylistDetailsComponent } from "../../ui/playlist-details/playlist-details.component";
+import { PlaylistDisplayComponent } from "../../ui/playlist-display/playlist-display.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [],
+  imports: [SearchBoxComponent, PlaylistDisplayComponent],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.scss",
 })
---
+++ src/app/ui/playlist-display/playlist-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlist-display.component.html',
+  styleUrl: './playlist-display.component.scss'
+})
+export class PlaylistDisplayComponent {
+
+}

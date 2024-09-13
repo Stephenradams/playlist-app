a34350773bb1dfbf02f89a904de3d2eb1b102215
Stephen Adams <stephen@cgcsoftware.co.uk>
2024-09-13 15:56:21 +0100

    feat: Add test:watch script to package.json

--- src/app/ui/navigation/navigation.component.spec.ts
+++ src/app/ui/navigation/navigation.component.spec.ts
@@ -1,23 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { NavigationComponent } from './navigation.component';
+import { NavigationComponent } from "./navigation.component";
+import { ActivatedRoute } from "@angular/router";
 
-describe('NavigationComponent', () => {
+describe("NavigationComponent", () => {
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavigationComponent]
-    })
-    .compileComponents();
+      imports: [NavigationComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { data: { title: "Test Title" } },
+          },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
